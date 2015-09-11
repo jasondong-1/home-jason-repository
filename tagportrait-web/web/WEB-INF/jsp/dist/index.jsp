cbbecf7f70608a311f1e65f0c9699a862dec1ac9
wanghuiren <wanghuiren23@126.com>
2015-09-11 22:06:52 +0800

    修改标签分布统计页面，引入地图组件。

---
+++ tagportrait-web/web/WEB-INF/jsp/dist/index.jsp
@@ -0,0 +1,129 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: whr
+  Date: 2015/9/11
+  Time: 21:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="/WEB-INF/include/taglib.jsp" %>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>text</title>
+  <meta name="description" content="Common form elements and layouts"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <!--[if IE 7]>
+  <link rel="stylesheet" href="${ctxRoot}/static/chosen/font-awesome-ie7.min.css"/>
+  <![endif]-->
+  <link rel="stylesheet" href="${ctxRoot}/static/framework/bootstrap/assets/css/chosen.css"/>
+  <link rel="stylesheet" href="${ctxRoot}/static/framework/bootstrap/assets/css/ace.css"/>
+  <!--[if lte IE 8]>
+  <link rel="stylesheet" href="${ctxRoot}/static/framework/bootstrap/assets/css/ace-ie.css"/>
+  <![endif]-->
+  <style>
+    .chosen-container {
+      margin-right: 15px;
+    }
+
+    .widget-box {
+      border: 0px;
+    }
+  </style>
+</head>
+<body>
+<div class="main-container" id="main-container">
+  <script type="text/javascript">
+    try {
+      ace.settings.check('main-container', 'fixed')
+    } catch (e) {
+    }
+  </script>
+  <div class="main-container-inner">
+    <div class="main-content">
+      <div class="page-content">
+        <div class="page-header">
+          <h1>
+            标签分布统计页面
+          </h1>
+        </div>
+        <!-- /.page-header -->
+        <div class="row">
+          <div class="col-xs-12">
+
+            <%--<form class="form-horizontal" role="form">--%>
+            <div class="row">
+              <div class="col-xs-10 col-xs-offset-1 ">
+                <div class="widget-box">
+                  <div class="widget-body">
+                    <div class="widget-main">
+                      <div>
+                        一级标签：
+                        <select class="width-15 chosen-select" id="form-field-select-1"
+                                data-placeholder="Choose a Country..." onchange="onFirstChange()">
+                          <option value="">请选择</option>
+                          <c:forEach var="item" items="${tagFirstList}">
+                            <option value="${item.id}">${item.name}</option>
+                          </c:forEach>
+                        </select>
+                        二级标签：
+                        <select class="width-15 chosen-select" id="form-field-select-2"
+                                data-placeholder="Choose a Country..." onchange="onSecondChange()">
+                          <option value="二级标签">请选择</option>
+                        </select>
+                        三级标签：
+                        <select class="width-15 chosen-select" id="form-field-select-3"
+                                data-placeholder="Choose a Country...">
+                          <%--<option value="">&nbsp;</option>--%>
+                          <option value="">请选择</option>
+                        </select>
+                        <button class="btn btn-sm btn-info"
+                                style="width: 80px;float: right">查 询
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-10 col-xs-offset-1">
+                <div id="map"
+                     style="width: 100%;height: 500px;border: 1px solid #d0d0d0;margin: 0px auto"></div>
+              </div>
+
+            </div>
+            <%--</form>--%>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<script src="${ctxRoot}/static/framework/bootstrap/assets/js/chosen.jquery.js"></script>
+<script src="${ctxRoot}/static/framework/echarts/echarts.js"></script>
+<script src="${ctxRoot}/static/js/dist.js"></script>
+<script type="text/javascript">
+  $(function() {
+    require.config({
+      paths: {
+        echarts: '${ctxRoot}/static/framework/echarts'
+      }
+    });
+    require(
+            [
+              'echarts',
+              'echarts/chart/map'  // 按需加载所需图表，如需动态类型切换功能，别忘了同时加载相应图表
+            ],
+            function (ec) {
+              var url = 'show_map_chart.do'
+              var data = {tagId: 1, areaId: 1};
+              X.post(url, data, mapCallback);
+            }
+    );
+  });
+</script>
+</body>
+</html>
+
+</body>
+</html>
---
+++ tagportrait-web/web/static/js/dist.js
@@ -0,0 +1,182 @@
+var option = {
+    title : {
+        text: '标签分布统计',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item'
+    },
+    legend: {
+        orient: 'vertical',
+        x:'left',
+        data:['iphone3','iphone4','iphone5']
+    },
+    dataRange: {
+        min: 0,
+        max: 2500,
+        x: 'left',
+        y: 'bottom',
+        text:['高','低'],           // 文本，默认为数值文本
+        calculable : true
+    },
+    roamController: {
+        show: true,
+        x: 'right',
+        mapTypeControl: {
+            'china': true
+        }
+    },
+    series : [
+        {
+            name: 'iphone3',
+            type: 'map',
+            mapType: 'china',
+            roam: false,
+            itemStyle:{
+                normal:{label:{show:true}},
+                emphasis:{label:{show:true}}
+            },
+            data:[
+                {name: '北京',value: Math.round(Math.random()*1000)},
+                {name: '天津',value: Math.round(Math.random()*1000)},
+                {name: '上海',value: Math.round(Math.random()*1000)},
+                {name: '重庆',value: Math.round(Math.random()*1000)},
+                {name: '河北',value: Math.round(Math.random()*1000)},
+                {name: '河南',value: Math.round(Math.random()*1000)},
+                {name: '云南',value: Math.round(Math.random()*1000)},
+                {name: '辽宁',value: Math.round(Math.random()*1000)},
+                {name: '黑龙江',value: Math.round(Math.random()*1000)},
+                {name: '湖南',value: Math.round(Math.random()*1000)},
+                {name: '安徽',value: Math.round(Math.random()*1000)},
+                {name: '山东',value: Math.round(Math.random()*1000)},
+                {name: '新疆',value: Math.round(Math.random()*1000)},
+                {name: '江苏',value: Math.round(Math.random()*1000)},
+                {name: '浙江',value: Math.round(Math.random()*1000)},
+                {name: '江西',value: Math.round(Math.random()*1000)},
+                {name: '湖北',value: Math.round(Math.random()*1000)},
+                {name: '广西',value: Math.round(Math.random()*1000)},
+                {name: '甘肃',value: Math.round(Math.random()*1000)},
+                {name: '山西',value: Math.round(Math.random()*1000)},
+                {name: '内蒙古',value: Math.round(Math.random()*1000)},
+                {name: '陕西',value: Math.round(Math.random()*1000)},
+                {name: '吉林',value: Math.round(Math.random()*1000)},
+                {name: '福建',value: Math.round(Math.random()*1000)},
+                {name: '贵州',value: Math.round(Math.random()*1000)},
+                {name: '广东',value: Math.round(Math.random()*1000)},
+                {name: '青海',value: Math.round(Math.random()*1000)},
+                {name: '西藏',value: Math.round(Math.random()*1000)},
+                {name: '四川',value: Math.round(Math.random()*1000)},
+                {name: '宁夏',value: Math.round(Math.random()*1000)},
+                {name: '海南',value: Math.round(Math.random()*1000)},
+                {name: '台湾',value: Math.round(Math.random()*1000)},
+                {name: '香港',value: Math.round(Math.random()*1000)},
+                {name: '澳门',value: Math.round(Math.random()*1000)}
+            ]
+        },
+        {
+            name: 'iphone4',
+            type: 'map',
+            mapType: 'china',
+            itemStyle:{
+                normal:{label:{show:true}},
+                emphasis:{label:{show:true}}
+            },
+            data:[
+                {name: '北京',value: Math.round(Math.random()*1000)},
+                {name: '天津',value: Math.round(Math.random()*1000)},
+                {name: '上海',value: Math.round(Math.random()*1000)},
+                {name: '重庆',value: Math.round(Math.random()*1000)},
+                {name: '河北',value: Math.round(Math.random()*1000)},
+                {name: '安徽',value: Math.round(Math.random()*1000)},
+                {name: '新疆',value: Math.round(Math.random()*1000)},
+                {name: '浙江',value: Math.round(Math.random()*1000)},
+                {name: '江西',value: Math.round(Math.random()*1000)},
+                {name: '山西',value: Math.round(Math.random()*1000)},
+                {name: '内蒙古',value: Math.round(Math.random()*1000)},
+                {name: '吉林',value: Math.round(Math.random()*1000)},
+                {name: '福建',value: Math.round(Math.random()*1000)},
+                {name: '广东',value: Math.round(Math.random()*1000)},
+                {name: '西藏',value: Math.round(Math.random()*1000)},
+                {name: '四川',value: Math.round(Math.random()*1000)},
+                {name: '宁夏',value: Math.round(Math.random()*1000)},
+                {name: '香港',value: Math.round(Math.random()*1000)},
+                {name: '澳门',value: Math.round(Math.random()*1000)}
+            ]
+        },
+        {
+            name: 'iphone5',
+            type: 'map',
+            mapType: 'china',
+            itemStyle:{
+                normal:{label:{show:true}},
+                emphasis:{label:{show:true}}
+            },
+            data:[
+                {name: '北京',value: Math.round(Math.random()*1000)},
+                {name: '天津',value: Math.round(Math.random()*1000)},
+                {name: '上海',value: Math.round(Math.random()*1000)},
+                {name: '广东',value: Math.round(Math.random()*1000)},
+                {name: '台湾',value: Math.round(Math.random()*1000)},
+                {name: '香港',value: Math.round(Math.random()*1000)},
+                {name: '澳门',value: Math.round(Math.random()*1000)}
+            ]
+        }
+    ]
+};
+
+$(function() {
+    $(".chosen-select").chosen();
+});
+function onFirstChange() {
+    var url='index_second.do'
+    var data={id: $("#form-field-select-1").val()};
+    X.post(url, data, callback_first)
+}
+
+function callback_first(r) {
+    var type = r['type'];
+    var data = r['data'];
+    var success = r['success'];
+    if(success){
+        //alert(JSON.stringify(r));
+        $("#form-field-select-2").empty();
+        $("#form-field-select-2").append("<option value=\"0\" selected=\"selected\">全部</option>");
+        for(var i=0;i<data.length;i++){
+            var item = data[i];
+            var option = "<option value='" + item.id +"'>" + item.name + "</option>";
+            $("#form-field-select-2").append(option);
+        }
+        $("#form-field-select-2").trigger("chosen:updated");
+    }
+}
+
+function onSecondChange() {//alert($("#form-field-select-1").val())
+    var url='index_third.do'
+    var data={id: $("#form-field-select-2").val()};
+    X.post(url, data, callback_second)
+}
+
+function callback_second(r) {
+    var type = r['type'];
+    var data = r['data'];
+    var success = r['success'];
+    if(success){
+        //alert(JSON.stringify(r));
+        $("#form-field-select-3").empty();
+        $("#form-field-select-3").append("<option value=\"0\" selected=\"selected\">全部</option>");
+        for(var i=0;i<data.length;i++){
+            var item = data[i];
+            var option = "<option value='" + item.id +"'>" + item.name + "</option>";
+            $("#form-field-select-3").append(option);
+        }
+        $("#form-field-select-3").trigger("chosen:updated");
+    }
+}
+
+function mapCallback(r) {
+    //alert(JSON.stringify(r));
+    var type = r['type'];
+    var data = r['data'];
+    var success = r['success'];
+    require('echarts').init(document.getElementById('map')).setOption(option);
+}
